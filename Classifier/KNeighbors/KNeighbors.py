990ad544e62065c0540daa3bded4f4d203eaab5a
Rhythm Shahriar <rhythm.shahriar@shohoz.net>
2018-09-26 17:09:32 +0600

    changes

--- Classifier/KNeighbors/KNeighbors.py
+++ Classifier/KNeighbors/KNeighbors.py
@@ -6,21 +6,22 @@ def GetVector(img):
     i = cv2.imread(img)
     i = cv2.resize(i, (224, 224))
     array = np.array(i)
+    vector = array.reshape(-1)
+    return vector
 
 
-    return array.reshape(-1)
 
 
 
+X = [GetVector('data/chair1.jpeg'),GetVector('data/chair2.jpeg'),GetVector('data/chair3.jpeg')]
 
-#print GetVector('im1.jpg')
-
-X = [GetVector('im1.jpg'),GetVector('im2.jpg')]
-y = [0, 1]
-
+y = ['chair-1', 'chair-2', 'chair-3']
 neigh = KNeighborsClassifier(n_neighbors=1)
-neigh.fit(X, y) 
+neigh.fit(X, y)
 
-print(neigh.predict([GetVector('test.jpeg')]))
+if neigh.predict_proba([GetVector('chair2.jpeg')])[0][1] >= 1.0:
+    print(neigh.predict([GetVector('chair2.jpeg')]))
+else:
+    print "No Match FOund"
 
 #print(neigh.predict_proba([[0.9]]))
