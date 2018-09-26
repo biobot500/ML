590addc8566944567165de1857ee3d29f3e4e10a
Rhythm Shahriar <rhythm.shahriar@shohoz.net>
2018-09-26 16:21:10 +0600

    changes

--- Classifier/KNeighbors/KNeighbors.py
+++ Classifier/KNeighbors/KNeighbors.py
@@ -1,9 +1,26 @@
-X = [[0,0,1], [1,0,0], [0,1,0], [1,1,0]]
-y = [0, 0, 1, 1]
 from sklearn.neighbors import KNeighborsClassifier
-neigh = KNeighborsClassifier(n_neighbors=3)
+import cv2
+import numpy as np
+
+def GetVector(img):
+    i = cv2.imread(img)
+    i = cv2.resize(i, (224, 224))
+    array = np.array(i)
+
+
+    return array.reshape(-1)
+
+
+
+
+#print GetVector('im1.jpg')
+
+X = [GetVector('im1.jpg'),GetVector('im2.jpg')]
+y = [0, 1]
+
+neigh = KNeighborsClassifier(n_neighbors=1)
 neigh.fit(X, y) 
 
-print(neigh.predict([[0,0,1]]))
+print(neigh.predict([GetVector('test.jpeg')]))
 
 #print(neigh.predict_proba([[0.9]]))
